8115971b9ac4db2639b9a12ef0d37b33fda7321b
Alina Gainanova <alinagainanova@MacBook-Pro-Alina.local>
2020-10-26 00:55:53 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let lastOpenBracket = '';
+    let allOpenBracket = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let p = 0; p < bracketsConfig.length; p++) {
+            if (str[i] == bracketsConfig[p][0] && str[i] == bracketsConfig[p][1] ) {
+                if (str[i] == lastOpenBracket) {
+                    allOpenBracket.pop();
+                    lastOpenBracket = allOpenBracket[allOpenBracket.length-1];
+                } else {
+                    lastOpenBracket = str[i];
+                    allOpenBracket.push(str[i]);
+                }
+                break
+            }
+            if (str[i] == bracketsConfig[p][0] && str[i] != bracketsConfig[p][1] ) {
+                lastOpenBracket = str[i];
+                allOpenBracket.push(str[i]);
+                break
+            }
+            if (str[i] == bracketsConfig[p][1] && allOpenBracket.length != 0) {
+                if (lastOpenBracket == bracketsConfig[p][0]) {
+                    allOpenBracket.pop();
+                    lastOpenBracket = allOpenBracket[allOpenBracket.length-1];
+                }
+                break
+            }
+            if (str[i] != bracketsConfig[p][0] && str[i] == bracketsConfig[p][1] && allOpenBracket.length == 0) {
+                return false
+            }
+        }
+    }
+    if (allOpenBracket.length == 0) {
+        return true
+    } else {
+        return false
+    }
 }
